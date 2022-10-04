856e17551a3ed3e6f217802fb3bb670b11413f41
MackyClap <martimac2320@gmail.com>
2022-10-03 21:45:50 -0400

    lint composer.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,7 @@
 $(document).ready(() => {
+
   $('#tweet-text').on('input', function(e) {
     let charNum = 140 - $(this).val().length;
     $('#counter').text(charNum);
-    
-  })
-
-
-})
+  });
+});
