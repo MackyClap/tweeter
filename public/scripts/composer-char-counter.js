283cf4d0fd3302c33d64de2a839d6555a190d138
MackyClap <martimac2320@gmail.com>
2022-10-06 19:46:06 -0400

    refactor composer-char-counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,11 @@
 $(document).ready(() => {
-
-  $('#tweet-text').on('input', function() {
+  $('#tweet-text').on('keyup', function() {
     let charNum = 140 - $(this).val().length;
     
 
     if (charNum >= 0) {
       $('#redCounter').attr('id', 'counter');
       $('#counter').text(charNum);
-      $('#redCounter').text(charNum);
     } else {
       $('#counter').attr('id', 'redCounter');
       $('#redCounter').text(charNum);
