fd02716f9d71893bcb88984cfe0f1435fa8612d5
MackyClap <martimac2320@gmail.com>
2022-10-07 21:25:21 -0400

    code hover state css

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(() => {
+
+  $(".tweet").hover(function () {
+
+    $(this).toggleClass('hover')
+  });
+
+  $("div.symbols").children().hover(function() {
+
+    $(this).toggleClass('symbols-hover')
+  });
+
+})
