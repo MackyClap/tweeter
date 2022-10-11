f93ead3b99f127c7e71927e94e2cd61e920cb773
MackyClap <martimac2320@gmail.com>
2022-10-10 20:48:52 -0400

    define rendertweets function & test

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,36 +6,49 @@
 
 $(document).ready(() => {
   
-  const tweetData =  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
- };
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
   
- 
- //createTweets and return the data
- const createTweetElement = (tweetData) => {
-
-    return $(`
+  
+  //createTweets and return the data
+  const createTweetElement = (obj) => {
+  
+    let $tweet = $(`
     <article class="tweet">
     <header>
     <div>
-    ${tweetData.user.name}
+    ${obj.user.name}
     </div>
     <div class="user-tag">
-    ${tweetData.user.handle}
+    ${obj.user.handle}
     </div>
     </header>
-    <span> ${tweetData.content.text}</span>
+    <span> ${obj.content.text}</span>
     
     <footer>
-    ${tweetData.created_at}
+    ${obj.created_at}
     <div class="symbols">
     <i class="fa-solid fa-flag"></i>
     <i class="fa-solid fa-repeat"></i>
@@ -43,21 +56,31 @@ $(document).ready(() => {
     </div>
     </footer>
     </article>`);
+  
+    return $tweet;
     
   };
   
-  
-  let $tweet = createTweetElement(tweetData);
-  
-  $(".tweets-container").append($tweet);
+  //definte function to take in array of tweets and append
+  const renderTweets = (tweetData) => {
+    for (const tweet of tweetData) {
+      let eachTweet = tweet;
+      //add tweets to tweets container
+      $(".tweets-container").append(createTweetElement(eachTweet));
+    }
+  };
 
-  $(".tweet").hover(function () {
+  renderTweets(tweetData);
 
-    $(this).toggleClass('hover')
+  //make box shadow on hover
+  $(".tweet").hover(function() {
+
+    $(this).toggleClass('hover');
   });
 
+  //make symbols below tweet change color
   $("div.symbols").children().hover(function() {
 
-    $(this).toggleClass('symbols-hover')
+    $(this).toggleClass('symbols-hover');
   });
 });
