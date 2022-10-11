ed87d17b48d25170c4f9efa87aa5332a565f4622
MackyClap <martimac2320@gmail.com>
2022-10-10 20:09:23 -0400

    contruct createTweetElement function w/o css

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,52 @@
  */
 
 $(document).ready(() => {
+  
+  const tweetData =  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+ };
+  
+ 
+ //createTweets and return the data
+ const createTweetElement = (tweetData) => {
+
+    return $(`
+    <article class="tweet">
+    <header>
+    <div>
+    Newton
+    </div>
+    <div class="user-tag">
+    @Mackattack
+    </div>
+    </header>
+    <span> I'm testing tweets here</span>
+    
+    <footer>
+    10 days ago
+    <div class="symbols">
+    <i class="fa-solid fa-flag"></i>
+    <i class="fa-solid fa-repeat"></i>
+    <i class="fa-solid fa-heart"></i>
+    </div>
+    </footer>
+    </article>`);
+    
+    
+  };
+  
+  
+  let $tweet = createTweetElement(tweetData);
+  
+  $(".tweets-container").append($tweet);
 
   $(".tweet").hover(function () {
 
@@ -15,5 +61,4 @@ $(document).ready(() => {
 
     $(this).toggleClass('symbols-hover')
   });
-
-})
+});
